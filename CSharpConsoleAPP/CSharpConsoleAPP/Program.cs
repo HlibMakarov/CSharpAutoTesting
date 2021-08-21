81622b1822f0488093fe1d7bdbfb2a5265408d9a
HlibMakarov <59768952+HlibMakarov@users.noreply.github.com>
2021-08-21 22:19:04 +0300

    Part 4
    video tutorial https://youtu.be/NNYJjn9b2-0

--- CSharpConsoleAPP/CSharpConsoleAPP/Program.cs
+++ CSharpConsoleAPP/CSharpConsoleAPP/Program.cs
@@ -1,12 +1,23 @@
 ﻿using System;
 
+
+// Создано по видеоуроку https://www.youtube.com/watch?v=0mm3djZ5KpQ&list=PL6tu16kXT9Pp3NFZgLbPZXEykeGQwxGSx&index=2&ab_channel=ExecuteAutomation
+
 namespace CSharpConsoleAPP
 {
     class Program
     {
+
         static void Main(string[] args)
         {
+            TestClass1 class1 = new TestClass1();
+
+            class1.TestCase1("PASSED");
+            class1.TestCase2("FAILED");
+
             Console.WriteLine("Hello World!");
+            Console.Read();
+
         }
     }
 }
---
+++ CSharpConsoleAPP/CSharpConsoleAPP/TestClass1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpConsoleAPP
+{
+    class TestClass1
+    {
+        //Members https://www.youtube.com/watch?v=NNYJjn9b2-0&list=PL6tu16kXT9Pp3NFZgLbPZXEykeGQwxGSx&index=4&ab_channel=ExecuteAutomation
+        public void TestCase1(string result)
+        {
+            Console.WriteLine("Executing TestCase1 "+ result);
+        }
+        public void TestCase2(string result)
+        {
+            Console.WriteLine("Executing TestCase2 " + result);
+        }
+    }
+}
