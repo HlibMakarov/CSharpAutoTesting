69d6c7a53e8aa2f0dbdb8f283827344f115d1f63
HlibMakarov <59768952+HlibMakarov@users.noreply.github.com>
2021-08-21 12:37:55 +0300

    Start#2

--- TestProjectCGleb/TestProjectCGleb/UnitTest1.cs
+++ TestProjectCGleb/TestProjectCGleb/UnitTest1.cs
@@ -1,12 +1,22 @@
 using NUnit.Framework;
-
-namespace TestProjectCGleb
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+namespace TestProject1
 {
     public class Tests
     {
+
+
         [SetUp]
         public void Setup()
         {
+            //Browser drivers
+            IWebDriver webDriver = new ChromeDriver();
+
+            //Navigate to site
+
+            webDriver.Navigate().GoToUrl("https://ledtechnics.dp.ua/ua/?utm_source=email2customer_look_companyname&utm_medium=Email&utm_campaign=after_order_cancel");
+
         }
 
         [Test]
